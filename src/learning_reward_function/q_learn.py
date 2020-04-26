3c2e775c2d4dbeb783319a49665f182ea861aaba
MuvvalaKaran <Karan.Muvvala@colorado.edu>
2020-04-25 19:06:38 -0600

    added policy plotting. Need to start intergeting the code from the two player games

--- src/learning_reward_function/q_learn.py
+++ src/learning_reward_function/q_learn.py
@@ -5,11 +5,16 @@ import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as anim
+import matplotlib.image as mping
+
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 
 # flag to plot while training
 plot_while_training = False
 # flaf to plot only after training
 plot_after_training = True
+# flag to add robot as patch
+use_robot_as_patch = True
 
 class GridWorld:
 
@@ -131,14 +136,18 @@ def play(env, agent, trials=500, max_steps_per_episode=1000, learn=False):
     reward_per_episode = []
     # initialize fig handle as None
     fig = None
+    # load robot_patch if need be
+    if use_robot_as_patch:
+        robot_patch = mping.imread("misc_files/robot_emoji_1.jpg")
+
     for trial in range(trials):
         if plot_while_training:
             # plot the basic gridworld
             if fig is None:
-                fig, ax = plot_gridworld_with_labels(env)
+                fig, ax = plot_gridworld_with_labels(env, num=f"Iteration # {trial}")
             else:
                 plt.close()
-                fig, ax = plot_gridworld_with_labels(env)
+                fig, ax = plot_gridworld_with_labels(env, num=f"Iteration # {trial}")
         cumulative_reward = 0
         step = 0
         game_over = False
@@ -154,7 +163,7 @@ def play(env, agent, trials=500, max_steps_per_episode=1000, learn=False):
             cumulative_reward += reward
             if plot_while_training:
                 # plot after each step
-                plot_agent_on_gridworld(ax, env.current_location)
+                plot_agent_on_gridworld(ax, env.current_location, image=robot_patch)
 
             step += 1
 
@@ -173,6 +182,8 @@ def roll_out_policy(agent, env):
     final_reward = 0
     # plot the basic gridworld
     fig, ax = plot_gridworld_with_labels(env)
+    if use_robot_as_patch:
+        robot_patch = mping.imread("misc_files/robot_emoji_1.jpg")
 
     # find the best action at the currest state, execute it do this repeatedly
     while 1:
@@ -186,35 +197,155 @@ def roll_out_policy(agent, env):
         curr_state = env.current_location
         # plot the final roll out policy
         if plot_after_training:
-            plot_agent_on_gridworld(ax, env.current_location, pause_time=1)
+            plot_agent_on_gridworld(ax, env.current_location, pause_time=0.1, image=robot_patch)
 
         final_reward += reward
 
         if env.check_state() == 'TERMINAL':
             print("Reached the final state")
+            plt.close()
+            fig, ax = plot_gridworld_with_labels(env)
+            plotQ(ax, env, agent.q_table, plot_square)
             break
 
+def load_robot_patch(xy ,robot_patch):
+    # lets try loading robot emoji as our controlled robot system
+    # robot_patch = mping.imread(file_name)
+    imageBox = OffsetImage(robot_patch, zoom=0.1)
+    ab = AnnotationBbox(imageBox, xy, frameon=False)
+    return ab
 
-def plot_agent_on_gridworld(ax, agent_xy, color=(0,1,0), pause_time=0.01):
+# def plot_labeled_arows(dirs):
+#     def draw(ax, xy, qs):
+#
+#         color_square(ax, xy, cm.coolwarm(max(qs)))
+#
+#         for q, direction in zip(qs, dirs):
+#             v = (q - min(qs))/ max(qs) - min(qs)
+#
+#             if not all(direction == CENTER):
+#                 plot_arrow()
+#             else:
+#                 plot_square()
+#
+#     return draw
+
+def normalize(qs):
+    # get the max and min q value
+    allq = [q for qs in qs.values() for q in qs.values()]
+    vmax, vmin = max(allq), min(allq)
+
+    range = max(vmax - vmin, 1e-8)
+
+    def norm(q):
+        return (q - vmin)/range
+
+    return {s: map(norm, a_value) for s, qs_values in qs.items() for a, a_value in qs_values.items()}
 
 
-    (x, y) = gridworld_to_xy(agent_xy[0], agent_xy[1])
+def plot_square(ax, xy, offset, color='green'):
+    x, y, = xy
+    # offset = offset * .47
+    # x = x + offset[0]*.15
+    # y = y + offset[1]*.15
 
-    # plot a circular agent in the girdworld
-    ax.add_patch(patches.Circle(
+    ax.add_patch(patches.Rectangle(
         (x, y),
-        radius=0.2,
-        color=color,
-        alpha=0.5  # transparency value
+        0.1,
+        0.1,
+        facecolor=color,
+        edgecolor='black',
+        alpha=0.5
     ))
 
+def plot_arrow(ax, xy, action, color='blue'):
+    x,  y = xy
+    if action == "RIGHT":
+        # draw upwards as left is upwards
+        dx = 0
+        dy = 0.3
+    elif action == "LEFT":
+        # draw downwards
+        dx = 0
+        dy = -0.3
+    elif action == "UP":
+        # draw left <---
+        dx = -0.3
+        dy = 0
+    else:
+        # draw right
+        dx = 0.3
+        dy = 0
+
+    ax.add_patch(patches.Arrow(
+        x, y, dx=dx, dy=dy, width=.3, facecolor=color
+    ))
+
+
+def get_best_Action(curr_state, q_table):
+    q_value_of_the_state = q_table[curr_state]
+    max_value = max(q_value_of_the_state.values())
+    action = np.random.choice([k for k, v in q_value_of_the_state.items() if v == max_value])
+    return action
+
+
+def plotQ(ax, gridworld, qs, plot_square):
+    # get the normalized values
+    # qs = normalize(qs)
+
+    for x in range(gridworld.width):
+        for y in range(gridworld.height):
+            xy = gridworld_to_xy(x, y)
+            # plot_square(ax, xy, qs[(x, y)])
+            color_square(ax, xy, color="green")
+            # find the optimal action and draw an arrow in that direction
+            best_action = get_best_Action(curr_state=(x, y), q_table=qs)
+            plot_arrow(ax, xy, best_action)
+            plt.draw()
+            plt.pause(0.1)
+
+    plt.show()
+
+def plot_agent_on_gridworld(ax, agent_xy, color="green", pause_time=0.01, **kwargs):
+    (x, y) = gridworld_to_xy(agent_xy[0], agent_xy[1])
+    robo_image = kwargs['image']
+    if use_robot_as_patch:
+        # # lets try loading robot emoji as our controlled robot system
+        # robot_patch = mping.imread('robot_emoji_1.jpg')
+        # imageBox = OffsetImage(robot_patch, zoom=0.1)
+        # ab = AnnotationBbox(imageBox, (x, y), frameon=False)
+
+        ax.add_artist(load_robot_patch((x,y),robo_image))
+    else:
+        # plot a circular agent in the girdworld
+        ax.add_patch(patches.Circle(
+            (x, y),
+            radius=0.2,
+            color=color,
+            alpha=1  # transparency value
+        ))
+
+        # ax.add_patch(patches.CirclePolygon(
+        #     (x, y),
+        #     radius=0.25,
+        #     color=color,
+        #     alpha=1,
+        # ))
+
+        # ax.add_patch(plt.Circle(
+        #     (x, y),
+        #     radius=0.25,
+        #     color=color,
+        #     alpha=1,
+        # ))
+    ax.set_aspect('equal')
     plt.draw()
     plt.pause(pause_time)
 
 
-
-def grid_plot(env):
-    fig = pl.figure()
+def grid_plot(env, *args):
+    fig_title = args[0]
+    fig = pl.figure(num=fig_title)
     ax = fig.add_subplot(111)
 
     cmax = env.width
@@ -249,8 +380,10 @@ def color_square(ax, xy, color):
         alpha=0.5  # transparency value
     ))
 
-def plot_gridworld_with_labels(env):
-    fig, ax = grid_plot(env)
+def plot_gridworld_with_labels(env, **kwargs):
+
+    plot_title = kwargs.get('num', 1)
+    fig, ax = grid_plot(env, plot_title)
     # lets annotate the gridworld
     for x in range(env.width):
         for y in range(env.height):
@@ -275,6 +408,10 @@ if __name__ == "__main__":
     reward_per_episode = play(env, agent, trials=500, learn=True)
     print("Done training \n")
     roll_out_policy(agent, env)
+    # plot the final policy learned
+
+
+
 
     plt.plot(reward_per_episode)
     # plot_gridworld_with_labels(env)
@@ -293,5 +430,6 @@ if __name__ == "__main__":
     #             color_square(ax, xy_coord, (255/ 255, 191/ 255, 0 / 255))
 
     # fig.show()
+
     plt.show()
 
